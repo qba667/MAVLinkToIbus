75443c795a46236cdaa89cf3312a8a64f9ccea7f
jakubk <jklimasz@gmail.com>
2016-12-16 23:29:57 +0100

    Base project license info added.

--- MAVLinkToIBUS/MAVLinkToIBUS.ino
+++ MAVLinkToIBUS/MAVLinkToIBUS.ino
@@ -1,3 +1,24 @@
+// iBus telemetry dongle converter for PIC 12F1572
+//
+// Copyright Dave Borthwick 2015
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//    
+// 
+// Multisensor implementation for Arduino Micro
+// Copyright Jakub Klimasz 2016
+
 #define MAVLINK_COMM_NUM_BUFFERS 1
 #include "TelemetryUtils.h"
 #include "mavlink_types.h"
